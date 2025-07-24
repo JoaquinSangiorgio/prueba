cdbbc54486f79889cb1167565634914d2698214e
JoaquinSangiorgio <sangiorgio.joaquin@uncuyo.edu.ar>
2025-07-24 14:12:13 -0300

    fix del index.js

--- backend/index.js
+++ backend/index.js
@@ -4,29 +4,24 @@ import dotenv from 'dotenv'
 
 import clientesRoutes from './routes/clientes.js'
 import notificarRoutes from './routes/notificar.js'
-app.use('/api/notificar', notificarRoutes)
-
 
 dotenv.config()
 
-const app = express()
+const app = express() 
 const PORT = process.env.PORT || 9001
-
 
 // Middlewares
 app.use(cors())
 app.use(express.json())
 
-// Ruta de prueba
+// Rutas
 app.get('/', (req, res) => {
   res.send('API funcionando correctamente')
 })
 
-
 app.use('/api/clientes', clientesRoutes)
 app.use('/api/notificar', notificarRoutes)
 
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`)
 })
